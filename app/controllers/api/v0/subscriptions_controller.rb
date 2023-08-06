7e72115d9675aeb3d7c9e129fff4332e5fe84dce
margdaly <margaret.daly@gmail.com>
2023-08-05 20:22:10 -0600

    feat: Add cancel/update subscription

--- app/controllers/api/v0/subscriptions_controller.rb
+++ app/controllers/api/v0/subscriptions_controller.rb
@@ -3,14 +3,25 @@ class Api::V0::SubscriptionsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_error_response
 
   def create
-    subscription = Subscription.create!(subscription_params)
+    subscription = Subscription.create!(create_params)
     render json: SubscriptionSerializer.new(subscription), status: 201
+  end
+
+  def update
+    # require 'pry'; binding.pry
+    subscription = Subscription.find_by!(update_params)
+    subscription.update!(status: false)
+    render json: SubscriptionSerializer.new(subscription), status: 200
   end
 
   private
 
-  def subscription_params
+  def create_params
     params.permit(:customer_id, :tea_id, :frequency)
+  end
+
+  def update_params
+    params.permit(:customer_id, :tea_id)
   end
 
   def render_error_response(exception)
